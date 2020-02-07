9d9efa7275151272a23fc184589dfd58cf23444d
Clivern <hello@clivern.com>
2020-02-07 15:04:00 +0100

    update

---
+++ src/Driver/Aggregator/AggregatorContract.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+ * This file is part of Metric - Metrics Collector SDK in PHP
+ * (c) Clivern <hello@clivern.com>
+ */
+
+namespace Clivern\Metric\Driver\Aggregator;
+
+/**
+ * Aggregator Contract.
+ */
+interface AggregatorContract
+{
+}
